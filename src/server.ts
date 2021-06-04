331f60336b442f44965d3139ba999779fa377a62
v <matheusyanmonteiro@gmail.com>
2021-06-04 15:22:06 -0300

    first struct basic complete

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
 app.get("/", (request, response) => {
-  return response.json({message: "starts"});
+  return response.json({
+    message: "Keep Calm 🚧 🚧 this app is all ready building ",
+  });
 });
 
-app.listen(3333);
+app.listen(3333, () => console.log("Server is Running! 🦝"));
