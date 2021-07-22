1c95b624dddde11acf63829faea6604b6480c6ab
v <matheusyanmonteiro@gmail.com>
2021-07-22 10:13:43 -0300

    created crypt for password

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -13,19 +13,24 @@ class UsersRepository implements IUsersRepository {
 
   async create({
     name,
-    username,
+    // username,
     email,
     driver_license,
     password,
   }: ICreateUserDTO): Promise<void> {
     const user = this.repository.create({
       name,
-      username,
+      // username,
       email,
       driver_license,
       password,
     });
     await this.repository.save(user);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    const user = await this.repository.findOne({ email });
+    return user;
   }
 }
 
--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcrypt";
 import { inject, injectable } from "tsyringe";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
@@ -12,16 +13,24 @@ class CreateUserUseCase {
 
   async execute({
     name,
-    username,
+    // username,
     email,
     password,
     driver_license,
   }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User Already exists!");
+    }
+
+    const passwordHash = await hash(password, 8);
+
     await this.usersRepository.create({
       name,
-      username,
+      // username,
       email,
-      password,
+      password: passwordHash,
       driver_license,
     });
   }
